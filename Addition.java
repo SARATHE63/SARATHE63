58b32c44dea2d0da61037a749baf79a4b091a645
SARATHE63 <109621006+SARATHE63@users.noreply.github.com>
2022-07-27 10:54:12 +0530

    Add to numbers with take user input.

---
+++ Addition.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Addition
+ {
+public static void main (String [] args)
+             {
+     int a,b,c;
+
+        Scanner sc=new Scanner(System.in);
+     a=sc.nextInt();
+          b=sc.nextInt();  
+         c=a+b;
+          System.out.println("sum="+c);
+   }
+       }                                                                                                                                                                                                
